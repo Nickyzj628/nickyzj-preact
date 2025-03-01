8c370fcfeaf6c3ca5e1d8e3ebc6a15a945ab2d4d
Nickyzj <1927139831@qq.com>
2025-03-01 21:48:35 +0800

    feat: 升级 Tailwind CSS v4

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from "path";
 import preact from "@preact/preset-vite";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +11,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "src"),
     },
   },
-  plugins: [preact(), svgr()],
+  plugins: [preact(), tailwindcss(), svgr()],
   build: {
     outDir: "D:/nginx/html",
   },
