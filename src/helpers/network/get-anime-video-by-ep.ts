a7550e9b950fa448ced7c9659a60fcea9ca63be3
Nickyzj <1927139831@qq.com>
2025-08-27 14:11:11 +0800

    fix: 修正更换 webdav 服务后的图片、视频地址

--- src/helpers/network/get-anime-video-by-ep.ts
+++ src/helpers/network/get-anime-video-by-ep.ts
@@ -3,5 +3,5 @@ import { ALIST_PORT, BASE_URL } from "@/constants";
 /** 从`nickyzj.run:2020/Nickyzj/Animes/{...anime}/{ep}`获取番剧播放地址 */
 export const getAnimeVideoByEP = (anime?: Anime, ep = 1) => {
   if (!anime || !anime.episodes) return;
-  return `${BASE_URL}:${ALIST_PORT}/d/Nickyzj/Animes/${anime.cate}/${anime.title}/${anime.episodes[ep - 1]}`;
+  return `${BASE_URL}:${ALIST_PORT}/Nickyzj/Animes/${anime.cate}/${anime.title}/${anime.episodes[ep - 1]}`;
 };
--- src/helpers/network/get-image.ts
+++ src/helpers/network/get-image.ts
@@ -2,5 +2,5 @@ import { ALIST_PORT, BASE_URL } from "@/constants";
 
 /** 从`nickyzj.run:2020/Photos{path}`获取图片，path以“/”开头 */
 export const getImage = (path: string) => {
-  return `${BASE_URL}:${ALIST_PORT}/d/Nickyzj/Photos${path}`;
+  return `${BASE_URL}:${ALIST_PORT}/Nickyzj/Photos${path}`;
 };
