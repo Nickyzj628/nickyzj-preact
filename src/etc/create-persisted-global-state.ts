1af06c09783257b0e8e53ab7e05f6e65532c6e93
Nickyzj <1927139831@qq.com>
2025-09-16 14:40:42 +0800

    refactor(createPersistedGlobalState): 优化初始值获取方式

--- src/etc/create-persisted-global-state.ts
+++ src/etc/create-persisted-global-state.ts
@@ -2,20 +2,25 @@ import { createGlobalState } from "react-use";
 
 /**
  * 在 react-use/createGlobalState 的基础上实现持久化缓存
+ * @param key 缓存键
+ * @param getInitialValue 获取初始值的函数
  * @example
- * const useUser = createPersistedGlobalState("user", { id: 1, name: "roxyzzz" });
+ * const useUser = createPersistedGlobalState("user", () => ({ id: 1, name: "roxyzzz" }));
  * const [user, setUser, removeUser] = useUser();
- * setUser({ id: 1, name: "roxy" });    // 缓存到 localStorage，供下次使用
- * removeUser();    // 清除缓存
+ * // 缓存到 localStorage，供下次使用
+ * setUser({ id: 1, name: "roxy" });
+ * // 清除缓存
+ * removeUser();
  */
-const createPersistedGlobalState = <T>(key: string, initialValue: T) => {
+const createPersistedGlobalState = <T>(key: string, getInitialValue: () => T) => {
     // 如果 localStorage 里没有值，则缓存初始值，供下次使用
-    const cachedValue = localStorage.getItem(key);
+    let cachedValue = localStorage.getItem(key);
     if (!cachedValue) {
-        localStorage.setItem(key, JSON.stringify(initialValue));
+        cachedValue = JSON.stringify(getInitialValue());
+        localStorage.setItem(key, cachedValue);
     }
 
-    const useGlobalValue = createGlobalState<T>(cachedValue ? JSON.parse(cachedValue) : initialValue);
+    const useGlobalValue = createGlobalState<T>(JSON.parse(cachedValue));
 
     return () => {
         const [value, setValue] = useGlobalValue();
--- src/hooks/store/use-user.ts
+++ src/hooks/store/use-user.ts
@@ -1,10 +1,9 @@
 import createPersistedGlobalState from "@/etc/create-persisted-global-state";
 import { random } from "@/helpers/number";
-import { nanoid } from "nanoid";
 
-const useUser = createPersistedGlobalState("user", {
-    id: nanoid(),
+const useUser = createPersistedGlobalState("user", () => ({
+    id: crypto.randomUUID(),
     name: `无名客${random(1000, 9999)}`,
-});
+}));
 
 export default useUser;
